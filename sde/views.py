ee3a718db174398ddc816845f21cadd6c5de876d
NeoHary <neohary@foxmail.com>
2020-07-07 17:30:33 +0800

    fix price api

--- sde/views.py
+++ sde/views.py
@@ -34,7 +34,7 @@ class itemListView(generic.ListView):
         prices = []
         stock = []
         for l in ls:
-            url = 'https://www.ceve-market.org/api/marketstat?typeid={0}&regionlimit=10000002'.format(l)
+            url = 'https://www.ceve-market.org/api/marketstat?typeid={0}&usesystem=30000142'.format(l)
             response = requests.get(url)
             data = xmltodict.parse(response.content)['evec_api']['marketstat']['type']['sell']['percentile']
             try:
